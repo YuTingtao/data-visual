29f1c3d4c3785e87028cd1e7c7cfd532b72f84eb
yutingtao <735126858@qq.com>
2024-05-23 10:23:40 +0800

    update

--- src/assets/map/province/index.js
+++ src/assets/map/province/index.js
@@ -1,172 +1,179 @@
+const modules = import.meta.glob('./*.json', { eager: true })
+const data = {}
+for (const path in modules) {
+  let key = path.replace(/(.*\/)*([^.]+).*/gi,'$2')
+  data[key] = modules[path].default
+}
+
 export default [
   {
     name: '北京',
     id: 110000,
-    data: require('./beijing.json'),
+    data: data.beijing,
   },
   {
     name: '天津',
     id: 120000,
-    data:  require('./tianjin.json'),
+    data:  data.tianjin,
   },
   {
     name: '河北',
     id: 130000,
-    data: require('./hebei.json'),
+    data: data.hebei,
   },
   {
     name: '山西',
     id: 140000,
-    data: require('./shanxi.json'),
+    data: data.shanxi,
   },
   {
     name: '内蒙古',
     id: 150000,
-    data: require('./neimenggu.json'),
+    data: data.shanxi,
   },
   {
     name: '辽宁',
     id: 210000,
-    data: require('./liaoning.json'),
+    data: data.liaoning,
   },
   {
     name: '吉林',
     id: 220000,
-    data: require('./jilin.json'),
+    data: data.jilin,
   },
   {
     name: '黑龙江',
     id: 230000,
-    data: require('./heilongjiang.json'),
+    data: data.heilongjiang,
   },
   {
     name: '上海',
     id: 310000,
-    data: require('./shanghai.json'),
+    data: data.shanghai,
   },
   {
     name: '江苏',
     id: 320000,
-    data: require('./jiangsu.json'),
+    data: data.jiangsu,
   },
   {
     name: '浙江',
     id: 330000,
-    data: require('./zhejiang.json'),
+    data: data.zhejiang,
   },
   {
     name: '安徽',
     id: 340000,
-    data: require('./anhui.json'),
+    data: data.anhui,
   },
   {
     name: '福建',
     id: 350000,
-    data: require('./fujian.json'),
+    data: data.fujian,
   },
   {
     name: '江西',
     id: 360000,
-    data: require('./jiangxi.json'),
+    data: data.jiangxi,
   },
   {
     name: '山东',
     id: 370000,
-    data: require('./shandong.json'),
+    data: data.shandong,
   },
   {
     name: '河南',
     id: 410000,
-    data: require('./henan.json'),
+    data: data.henan,
   },
   {
     name: '湖北',
     id: 420000,
-    data: require('./hubei.json'),
+    data: data.hubei,
   },
   {
     name: '湖南',
     id: 430000,
-    data: require('./hunan.json'),
+    data: data.hunan,
   },
   {
     name: '广东',
     id: 440000,
-    data: require('./guangdong.json'),
+    data: data.guangdong,
   },
   {
     name: '广西',
     id: 450000,
-    data: require('./guangxi.json'),
+    data: data.guangxi,
   },
   {
     name: '海南',
     id: 460000,
-    data: require('./hainan.json'),
+    data: data.hainan,
   },
   {
     name: '重庆',
     id: 500000,
-    data: require('./chongqing.json'),
+    data: data.chongqing,
   },
   {
     name: '四川',
     id: 510000,
-    data: require('./sichuan.json'),
+    data: data.sichuan,
   },
   {
     name: '贵州',
     id: 520000,
-    data: require('./guizhou.json'),
+    data: data.guizhou,
   },
   {
     name: '云南',
     id: 530000,
-    data: require('./yunnan.json'),
+    data: data.yunnan,
   },
   {
     name: '西藏',
     id: 540000,
-    data: require('./xizang.json'),
+    data: data.xizang,
   },
   {
     name: '陕西',
     id: 610000,
-    data: require('./shanxi1.json'),
+    data: data.shanxi1,
   },
   {
     name: '甘肃',
     id: 620000,
-    data: require('./gansu.json'),
+    data: data.gansu,
   },
   {
     name: '青海',
     id: 630000,
-    data: require('./qinghai.json'),
+    data: data.qinghai,
   },
   {
     name: '宁夏',
     id: 640000,
-    data: require('./ningxia.json'),
+    data: data.ningxia,
   },
   {
     name: '新疆',
     id: 650000,
-    data: require('./xinjiang.json'),
+    data: data.xinjiang,
   },
   {
     name: '香港',
     id: 810000,
-    data: require('./xianggang.json'),
+    data: data.xianggang,
   },
   {
     name: '澳门',
     id: 820000,
-    data: require('./aomen.json'),
+    data: data.aomen,
   },
   {
     name: '台湾',
     id: 710000,
-    data: require('./taiwan.json'),
+    data: data.taiwan,
   },
 ]
