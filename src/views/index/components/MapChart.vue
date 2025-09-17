ff23e7f7a7ba8272195428684a6dbb48b430dddd
余廷涛 <735126858@qq.com>
2025-09-17 21:05:39 +0800

    update

--- src/views/index/components/MapChart.vue
+++ src/views/index/components/MapChart.vue
@@ -131,13 +131,17 @@ const loading = ref(false);
 // 加载地图
 function loadMap(name) {
   loading.value = true;
-  axios.get(`${import.meta.env.BASE_URL}./map/province/${name}.json`).then(res => {
-    registerMap(name, res.data);
-    option.value.geo.map = name;
-    option.value.series[0].data = [];
-  }).finally(() => {
-    loading.value = false;
-  });
+  const baseUrl = import.meta.env.BASE_URL === '/' ? '.' : import.meta.env.BASE_URL;
+  axios
+    .get(`${baseUrl}/map/province/${name}.json`)
+    .then(res => {
+      registerMap(name, res.data);
+      option.value.geo.map = name;
+      option.value.series[0].data = [];
+    })
+    .finally(() => {
+      loading.value = false;
+    });
 }
 
 // 返回中国地图
